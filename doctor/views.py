67ef248117fdb9b69468f65dd15101877594b681
Shaha00 <ahmadoshahzod438@gmail.com>
2023-03-20 22:13:37 +0600

    Test Api

--- doctor/views.py
+++ doctor/views.py
@@ -1,10 +1,9 @@
 from requests import Response
-from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
 from .models import Review, Doctor, Record
 from .serializers import ReviewSerializer, DoctorSerializer, RecordSerializer, DoctorValidateSerializer, DoctorDetailSerializer
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework import status
 from rest_framework.permissions import IsAdminUser
 
